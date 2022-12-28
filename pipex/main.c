c16e2f11b3568687a582edf40b05d1f57859a67f
David LY <dly@e2r5p22.clusters.42paris.fr>
2022-12-28 18:42:01 +0100

    save

--- pipex/main.c
+++ pipex/main.c
@@ -6,17 +6,65 @@
 /*   By: dly <dly@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 20:23:48 by dly               #+#    #+#             */
-/*   Updated: 2022/12/26 19:51:08 by dly              ###   ########.fr       */
+/*   Updated: 2022/12/28 18:32:20 by dly              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(int ac, char **av)
+#include "pipex.h"
+
+int	p_err()
 {
-	int	fd1;
-	int	fd2;
+	write(2, "Err\n", 4);
+	return (1);
+}
+
+void	p_err_()
+{
+	exit(EXIT_FAILURE);
+}
+
+int	main(int ac, char **av, char **envp)
+{
+	t_pipex	pipex;
 	
 	if (ac != 5)
 		return (0);
-	fd1 = 
+	pipex.infile = open(av[1], O_RDONLY);
+	if (pipex.infile < 0)
+	{
+		write(2, "Error\n", 6);
+		exit(EXIT_FAILURE);
+	}
+	pipex.outfile = open(av[3], O_RDWR | O_CREAT | O_TRUNC, 0644);
+	if (pipex.outfile < 0)
+		return (p_err);
+	pipex.envp_path = get_path
+	//need path / cmd / 
+	if (pipe(pipex.end) < 0)
+		return (p_err);
+	
+	if (close(pipex.infile) < 0)
+	{
+		write(2, "Error\n", 6);
+		exit(EXIT_FAILURE);
+	}
 	return (0);
+}
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	while ((*s1 || *s2) && (n > 0))
+	{
+		if (*s1 != *s2)
+			return ((unsigned char)*s1 - (unsigned char)*s2);
+		s1++;
+		s2++;
+		n--;
+	}
+	return (0);
+}
+char	*find_path(char **envp)
+{
+	while (ft_strncmp("PATH", *envp, 4))
+		envp++;
+	return (*envp + 5);
 }
--- pipex/pipex.h
+++ pipex/pipex.h
@@ -3,16 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dly <marvin@42.fr>                         +#+  +:+       +#+        */
+/*   By: dly <dly@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 20:25:18 by dly               #+#    #+#             */
-/*   Updated: 2022/12/23 20:26:00 by dly              ###   ########.fr       */
+/*   Updated: 2022/12/28 18:31:41 by dly              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include "libft.h"
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+typedef struct s_pipex
+{
+	int		infile;
+	int		outfile;
+	int		end[2];
+	char	*cmd;
+	char	*envp_path;
+	char	**my_paths;
+	char	**cmd_args;
+}	t_pipex;
 
 #endif
