52366df179d02151e7d3ff34c24c9cb857f56bbc
David LY <dly@e2r4p18.clusters.42paris.fr>
2023-01-04 20:04:03 +0100

    save

--- pipex/include/pipex.h
+++ pipex/include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: dly <dly@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 20:25:18 by dly               #+#    #+#             */
-/*   Updated: 2023/01/03 17:53:09 by dly              ###   ########.fr       */
+/*   Updated: 2023/01/04 18:16:45 by dly              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 typedef struct s_pipex
 {
-	pid_t	pid1;
-	pid_t	pid2; 
+	pid_t	child1;
+	pid_t	child2; 
 	int		infile;
 	int		outfile;
 	int		end[2];
@@ -40,5 +42,11 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	**ft_split(char const *s, char c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_join_three(char *str1, char *str2, char *str3);
+void	child_one(t_pipex pipex, char **av, char **envp);
+void	child_two(t_pipex pipex, char **av, char **envp);
+void	free_child(t_pipex *pipex, char *msg);
+void	free_parent(t_pipex *pipex);
+void	p_err_(char *msg);
 
 #endif
--- pipex/srcs/mandatory/main.c
+++ pipex/srcs/mandatory/main.c
@@ -6,21 +6,29 @@
 /*   By: dly <dly@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 20:23:48 by dly               #+#    #+#             */
-/*   Updated: 2023/01/03 19:08:23 by dly              ###   ########.fr       */
+/*   Updated: 2023/01/04 20:03:50 by dly              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	p_err(char *err_msg)
+void	p_err_(char *msg)
 {
-	write(2, err_msg, ft_strlen(err_msg));
-	return (1);
+	perror(msg);
+	exit(EXIT_FAILURE);
 }
 
-void	p_err_()
+char	*get_path(char **envp)
 {
-	exit(EXIT_FAILURE);
+	while (ft_strncmp("PATH", *envp, 4))
+		envp++;
+	return (*envp + 5);
+}
+
+void	close_pipe(t_pipex *pipex)
+{
+	if (close(pipex->end[0]) || close(pipex->end[1]))
+		p_err_("ERR_CLOSE");
 }
 
 int	main(int ac, char **av, char **envp)
@@ -30,38 +38,31 @@ int	main(int ac, char **av, char **envp)
 	(void)av;
 	
 	if (ac != 5)
-		return (0);
+		return (1);
 	pipex.infile = open(av[1], O_RDONLY);
 	if (pipex.infile < 0)
-		return (p_err("err\n"));
-	pipex.outfile = open(av[3], O_RDWR | O_CREAT | O_TRUNC, 0644);
+		p_err_("ERR_OPEN");
+	pipex.outfile = open(av[4], O_RDWR | O_CREAT | O_TRUNC, 0644);
 	if (pipex.outfile < 0)
-		return (p_err("err\n"));
+		p_err_("ERR_OPEN");
 	pipex.envp_path = get_path(envp);
 	pipex.cmd_paths = ft_split(pipex.envp_path, ':');
+	if (pipex.cmd_paths == NULL)
+		p_err_("ERR_MALLOC");
 	if (pipe(pipex.end) < 0)
-		return (p_err);
-	pipex.pid1 = fork();
-	if (pipex.pid1 == 0)
 	{
-			
+		free_parent(&pipex);
+		p_err_("ERR_PIPE");
 	}
-	pipex.pid2 = fork();
-	if (pipex.pid2 == 0)
-	{
-		
-	}
-	if (close(pipex.infile) < 0)
-	{
-		write(2, "Error\n", 6);
-		exit(EXIT_FAILURE);
-	}
+	pipex.child1 = fork();
+	if (pipex.child1 == 0)
+		child_one(pipex, av, envp);
+	pipex.child2 = fork();
+	if (pipex.child2 == 0)
+		child_two(pipex, av, envp);
+	close_pipe(&pipex);
+	waitpid(pipex.child1, NULL, 0);
+	waitpid(pipex.child2, NULL, 0);
+	free_parent(&pipex);
 	return (0);
-}
-
-char	*get_path(char **envp)
-{
-	while (ft_strncmp("PATH", *envp, 4))
-		envp++;
-	return (*envp + 5);
 }
